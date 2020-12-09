8414f108d6882ce02fd3fe1dc8556386ccf5be18
Julia-Davydiuk <70822407+Julia-Davydiuk@users.noreply.github.com>
2020-12-09 21:12:22 +0100

    2

--- js/task-2_1.js
+++ js/task-2_1.js
@@ -11,12 +11,18 @@
 
 const friends = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
 const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+let messange;
 
 const logItems = function(array) {
     for (let i = 0; i < friends.length; i += 1) {
-        console.log(friends[i]);
+        messange = `${i + 1} - ${friends[i]}`;
+        console.log(messange);
       }
-      
+    
+    for (let i = 0; i < numbers.length; i += 1){
+        messange = `${i + 1} - ${numbers[i]}`;
+        console.log(messange);
+      }  
 };
 
 logItems();
