c81bbaade1470d08c31ade59218bf4bf8a31c6f0
Julia-Davydiuk <70822407+Julia-Davydiuk@users.noreply.github.com>
2020-12-26 18:04:45 +0100

    изменение в 4 задаче.
     Упрощонный вариант

--- js/task-2_4.js
+++ js/task-2_4.js
@@ -5,6 +5,10 @@
 // Если длина больше 40 символов, то функция обрезает строку до 40-ка символов и 
 // добавляет в конец строки троеточие '...', после чего возвращает укороченную версию.
 
+// const formatString = function(string) {
+//   return string.length <= 40? string : `${string.slice(0, 40)}...`;
+//   };
+
 const formatString = function(string) {
 
   if (string.length > 40) {
